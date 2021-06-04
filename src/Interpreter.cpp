f187b178818a39f7b1b1bce52151668f9989ece5
Lumos-az <angzhan.he@zju.edu.cn>
2021-06-04 10:43:08 +0800

    Init

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -2,3 +2,15 @@
 // Created by 何昂展 on 2021/6/4.
 //
 
+#include "Interpreter.h"
+#include <string>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void Interpreter::MainPage() {
+    cout << "Welcome to Our MiniSQL!" << endl;
+    string line = "";
+    string
+}
--- src/Interpreter.h
+++ src/Interpreter.h
@@ -5,4 +5,15 @@
 #ifndef MINISQL_INTERPRETER_H
 #define MINISQL_INTERPRETER_H
 
+#include <string>
+#include <vector>
+
+class Interpreter {
+public:
+    void MainPage();
+    Interpreter();
+    ~Interpreter();
+
+};
+
 #endif //MINISQL_INTERPRETER_H
